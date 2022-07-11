77e6af807251bcd7b454ee0b5ebe34a71a325b13
adamskirees <adamskirees@yahoo.com>
2022-07-11 11:03:53 +1000

    first commit , set up files

--- website/app.js
+++ website/app.js
@@ -1,5 +1,5 @@
 /* Global Variables */
 
-// Create a new date instance dynamically with JS, check for more comments ? 
+// Create a new date instance dynamically with JS, check for more comments ? ?? 
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
