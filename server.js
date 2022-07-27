394d2b0728d3e7e74d42867dfa22179bbf4d74bb
adamskirees <adamskirees@yahoo.com>
2022-07-27 20:54:39 +1000

    Restarted, check server running

--- server.js
+++ server.js
@@ -2,18 +2,37 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require("express");
 
 // Start up an instance of app
+const app = express();
 
-/* Middleware*/
+/* Middleware-Dependencies*/
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({
+    extended: false,
+})
+);
+
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require("cors");
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const port = 3000;
+
+// Spin up the server
+const server = app.listen(port, listening);
+//test if working
+function listening() {
+    console.log("server running");
+    console.log(`running on localhost ${port}`);
+  }
+
+  
--- website/app.js
+++ website/app.js
@@ -1,5 +1,4 @@
-/* Global Variables */
-
-// Create a new date instance dynamically with JS, check for more comments ? ?? ??
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+// Personal API Key for OpenWeatherMap API
+const apiKey = "&appid=4d4a3eb9d322124aa8855cbdec9d1253";
+const baseURL =
+  "http://api.openweathermap.org/data/2.5/weather?units=imperial&zip=";
