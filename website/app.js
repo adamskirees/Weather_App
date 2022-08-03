7e6afe5a2db2da194ed629c1699e277ad300635a
adamskirees <adamskirees@yahoo.com>
2022-08-03 20:37:36 +1000

    Bring in event listener for button

--- website/app.js
+++ website/app.js
@@ -2,3 +2,31 @@
 const apiKey = "&appid=4d4a3eb9d322124aa8855cbdec9d1253";
 const baseURL =
   "http://api.openweathermap.org/data/2.5/weather?units=imperial&zip=";
+
+  //Set up event listener
+  let start = document.getElementById("generate").addEventListener("click", requestAction);
+  
+  //Bring in event listener
+  let requestAction = (e) => {
+    let zipcode = document.getElementById("zip").value;
+    let feelings = document.getElementById("feelings").value;
+    getWeather(`${baseURL}${zipcode}${apiKey}`).then(function (data) {
+      postData("(http://localhost:3000/show", {
+        temp: data.main.temp,
+        content: feelings,
+      }).then(updateUI);
+    });
+  };
+
+
+
+  //GET function - to pull weather API
+  let getWeather = async (url) => {
+    let response = await fetch(url);
+    try {
+      let data = await response.json();
+      HTMLFormControlsCollection.log(data);
+    } catch (error) {
+      console.log("error", error);
+    }
+    };
